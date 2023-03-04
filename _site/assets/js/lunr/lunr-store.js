3705077b364352401d44c732f40e2b91dddefb0c
Kutay Bozkurt <kutay@kutay-2.local>
2023-03-04 18:19:35 +0300

    added analytics.html

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,5 +3,5 @@ var store = [{
         "excerpt":"Sir Potata: Hello and welcome to The Cookie Blog. This is where we, two (nerdy) high-school students, voice our opinions about AI art, have fun with it and explain to you readers about what is all the deal going around it. We hope to learn many things, discover ways to...","categories": ["intro"],
         "tags": [],
         "url": "/intro/2023/03/01/welcome-to-cookie-blog.html",
-        "teaser": null
+        "teaser": "/assets/images/cyberpunk-japanese.png"
       }]
