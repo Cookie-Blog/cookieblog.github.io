046f0b393394ff2d7b8072cd7e4f7994de13537a
Kutay Bozkurt <kutay@kutay-2.local>
2023-03-04 17:25:11 +0300

    added minimal-mistakes theme

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Welcome To Our Cookie Blog",
+        "excerpt":"Sir Potata: Hello and welcome to The Cookie Blog. This is where we, two (nerdy) high-school students, voice our opinions about AI art, have fun with it and explain to you readers about what is all the deal going around it. We hope to learn many things, discover ways to...","categories": ["intro"],
+        "tags": [],
+        "url": "/intro/2023/03/01/welcome-to-cookie-blog.html",
+        "teaser": null
+      }]
